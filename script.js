c32d863c62119b62df9e0b35479f297f5a2c7374
grizelleMenezes <grizellem15@gmail.com>
2018-07-14 14:31:37 +0530

    Got player to move

---
+++ script.js
@@ -0,0 +1,36 @@
+var game = new Phaser.Game(
+		800,
+		600,
+		Phaser.AUTO,
+		'',
+		{
+			preload: preload,
+			create: create,
+			update: update
+		}
+	)
+var ship;
+var cursor;
+function preload(){
+	game.load.image("sky", "assets/invaders/sky.png")
+	game.load.image("ship", "assets/invaders/player.png")
+}
+
+function create(){
+	game.add.tileSprite(0, 0, 800, 600 ,'sky')
+    ship = game.add.sprite(375, 300 ,'ship')
+    game.physics.arcade.enable(ship)
+    ship.body.collideWorldBounds = true;
+    cursor = game.input.keyboard.createCursorKeys()
+}
+
+function update(){
+	if(cursor.left.isDown){
+		console.log("left pressed")
+		ship.body.velocity.x = -25;
+	}
+	else if(cursor.right.isDown){
+		console.log("right pressed")
+		ship.body.velocity.x = +25;
+	}
+}
