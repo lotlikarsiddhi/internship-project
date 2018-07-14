fd2e5f44716cf64edef527e90fa75b439a9d8aa4
grizelleMenezes <grizellem15@gmail.com>
2018-07-14 16:20:01 +0530

    added shipFire() function

--- script.js
+++ script.js
@@ -13,9 +13,13 @@ var ship;
 var cursor;
 var sky;
 var fireButton;
+var bullets;
+var bulletTime = 0;
+
 function preload(){
 	game.load.image("sky", "assets/invaders/sky.png")
 	game.load.image("ship", "assets/invaders/player.png")
+	game.load.image("bullet", "assets/invaders/bullet.png")
 }
 
 function create(){
@@ -25,6 +29,16 @@ function create(){
     game.physics.arcade.enable(ship)
     ship.body.collideWorldBounds = true;
     cursor = game.input.keyboard.createCursorKeys()
+
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(30, 'bullet')
+    bullets.setAll('anchor.x', 0.5)
+    bullets.setAll('anchor.y', 1)
+
+    bullets.setAll('checkWorldBounds', true);
+    bullets.setAll('outOfBoundsKill', true);
 
     fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 }
@@ -43,8 +57,16 @@ function update(){
 	if(fireButton.isDown){
 		shipFire();
 	}
-}
+}0
 
  function shipFire(){
  	console.log("Bullet was fired")
+ 	if(game.time.now > bulletTime ){
+ 		bullet = bullets.getFirstExists(false)
+ 		if(bullet){
+ 			bullet.reset(ship.x, ship.y + 8);
+ 			bullet.body.velocity.y = -400;
+ 			bulletTime = game.time.now + 200;
+ 		}
+ 	}
  }
